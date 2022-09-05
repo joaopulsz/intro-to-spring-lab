0c906c1872c72d4ddd0f97e8c419826756d40116
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-09-05 14:48:28 +0100

    finished mvp

---
+++ src/main/java/com/example/task_01/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.task_01.controllers;
+
+import com.example.task_01.models.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public String newGreeting(@RequestParam(value = "name") String name, @RequestParam(value = "timeOfDay") String timeOfDay) {
+        Greeting greeting = new Greeting(name, timeOfDay);
+        return "Good " + greeting.getTimeOfDay() + ", " + greeting.getName() + "!";
+    }
+
+}
