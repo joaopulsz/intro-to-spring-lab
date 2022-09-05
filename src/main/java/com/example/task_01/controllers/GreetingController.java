f8c7ac34092a8a45ca56c6617c257f3d6ee471b8
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-09-05 15:09:13 +0100

    finished extensions

--- src/main/java/com/example/task_01/controllers/GreetingController.java
+++ src/main/java/com/example/task_01/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.task_01.controllers;
 
+import com.example.task_01.models.Celebration;
 import com.example.task_01.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,11 @@ public class GreetingController {
     public String newGreeting(@RequestParam(value = "name") String name, @RequestParam(value = "timeOfDay") String timeOfDay) {
         Greeting greeting = new Greeting(name, timeOfDay);
         return "Good " + greeting.getTimeOfDay() + ", " + greeting.getName() + "!";
+    }
+
+    @GetMapping(value = "/christmas")
+    public Celebration newCelebration() {
+        return new Celebration("Merry Christmas!");
     }
 
 }
